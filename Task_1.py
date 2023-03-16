fc5fc4431d3bf66817570a9526dd2ca81a93da55
Mariya <appleslover@yandex.ru>
2023-03-16 22:34:33 +0300

    ДЗ № 2 Питон

--- Task_1.py
+++ Task_1.py
@@ -12,12 +12,17 @@ TailCount = 0
 HeadCount = 0
 while count < N:
     CoinSide = randint(0,1)
+    print(CoinSide)
     if CoinSide == 0:
         TailCount += 1
     elif CoinSide == 1:
         HeadCount += 1
+    count += 1 
+
+print(f"Решкой вверх лежит {TailCount} монет")
+print(f"Орлом вверх лежит {HeadCount} монет")       
 
 if TailCount < HeadCount:
     print(f"Необходимо перевернуть {TailCount} монет")
 else:
-    print(f"Необходимо перевернуть {HeadCount} монет")
+    print(f"Необходимо перевернуть {HeadCount} монеты")
